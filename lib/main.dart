1212c281e56b19ac5ff9a2fe08d2acb478bb06de
quannm-amcolab <quannm@amcolab.vn>
2025-12-05 11:27:12 +0700

    Update MyHomePage title and increment counter by 2 for enhanced functionality.

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page 1'),
     );
   }
 }
@@ -33,7 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      _counter++;
+      _counter += 2;
     });
   }
 
